a19070c35d29f387fd7bf024865c435e902efabb
Dylan Glover <dylanglover6@gmail.com>
2018-02-27 00:16:57 -0600

    finished coding game and styled

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,41 @@
 //designates score target number
-var targetNumber = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58];
 function chooseTarget(){
-    return targetNumber[Math.floor(Math.random() * targetNumber.length)];
+    return Math.floor(Math.random() * 101) + 19;
  };
 var randomTarget = chooseTarget();
 $("#number-to-guess").text(randomTarget);
 
 //global variables
 var counter = 0;
-var numberOptions = [10, 5, 12, 3, 7, 9, 2, 4, 5, 6, 11];
-var crystalImages= ["./assets/images/purple-crystal.png", "./assets/images/green-crystal.png", "./assets/images/orange-crystal.png", "./assets/images/teal-crystal.png", "./assets/images/sparkly-crystal.png", "./assets/images/lime-crystal.png", "./assets/images/diamond-crystal.png", "./assets/images/red-crystal.png",]
+var numberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+var crystalImages= ["./assets/images/purple-crystal.png", "./assets/images/orange-crystal.png", "./assets/images/teal-crystal.png", "./assets/images/sparkly-crystal.png", "./assets/images/lime-crystal.png", "./assets/images/diamond-crystal.png", "./assets/images/red-crystal.png",]
 var wins = 0;
 var losses = 0;
+var temples = [".temple-1", ".temple-2", ".temple-3", ".temple-4"]
 
+//code to position jewels on top of temples
+$.fn.positionOn = function(element, align) {
+    return this.each(function() {
+      var target   = $(this);
+      var position = element.position();
+  
+      var x      = position.left; 
+      var y      = position.top;
+  
+      if(align == 'right') {
+        x -= (target.outerWidth() - element.outerWidth());
+      } else if(align == 'center') {
+        x -= target.outerWidth() / 2 - element.outerWidth() / 2;
+      }
+  
+      target.css({
+        position: 'absolute',
+        zIndex:   5000,
+        top:      y, 
+        left:     x
+      });
+    });
+  };
 
 //for loop to create crystals and assign values
 function createCrystals () {
@@ -21,6 +44,7 @@ function createCrystals () {
     var randomNumber = Math.floor(Math.random()*numberOptions.length);
     var randomCrystal = Math.floor(Math.random()* crystalImages.length);
     imageCrystal.addClass("crystal-image");
+    imageCrystal.positionOn($(temples[i]), "center")
     imageCrystal.attr("src", crystalImages[randomCrystal]);
     imageCrystal.attr("data-crystalvalue", numberOptions[randomNumber]);
     $("#crystals").append(imageCrystal);
@@ -33,6 +57,7 @@ function reset() {
     $("#crystals").empty();
     createCrystals();
     counter = 0;
+    randomTarget = chooseTarget();
     $("#number-to-guess").text(randomTarget);
     $(".crystal-image").on("click", function() {
         //determines the value of crystal and converts to an integer
@@ -44,14 +69,14 @@ function reset() {
         alert("New score: " + counter);
     
         if (counter === randomTarget) {
-            alert("you win!");
+            alert("You Win!");
             reset();
             wins ++;
             $("#wins").html(wins);
         }
     
         else if (counter >= randomTarget) {
-            alert("you lose!");
+            alert("You Lose!");
             reset();
             losses ++;
             $("#losses").html(losses);
